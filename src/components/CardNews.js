d57b92e8db4c2c78fdb0b94f68551d1201201e13
Lucas Gonçalves <103429796+Lucc-Minerva@users.noreply.github.com>
2023-09-29 16:58:40 +0000

    transformando o conteúdodem HTM em um Componente fantasma

--- src/components/CardNews.js
+++ src/components/CardNews.js
@@ -2,8 +2,32 @@ class CardNews extends HTMLElement{
     constructor() {
         super();
 
-        const shadow = this.attachShadow({mode: "open"})
-        shadow.innerHTML = "<h1>Olá Mundo!</h1>";
+        const shadow = this.attachShadow({mode: "open"});
+        shadow.appendChild(this.build());
+        shadow.appendChild(this.styles());
+    }
+
+    build(){
+        // div pai, ou no caso a div principal
+        const componentRoot = document.createElement ("div");
+        // atribuindo 
+        componentRoot.setAttribute("class", "card");
+
+        const cardLeft = document.createElement ("div");
+        cardLeft.setAttribute("class", "card_left");
+
+        const cardRight = document.createElement ("div");
+        cardRight.setAttribute("class", "card_right");
+
+        // prendendo os dois componentes cardLeft e cardRight no component pai, no caso: componentRoot
+        componentRoot.appendChild(cardLeft);
+        componentRoot.appendChild(cardRight);
+
+        return componentRoot;
+    }
+
+    styles(){
+
     }
 }
 
